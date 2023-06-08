2df8c0963b9501e82225172c9a036cc03785cdc3
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-06-08 17:20:51 +0530

    home page ui top part finished

--- lib/Constants/constants.dart
+++ lib/Constants/constants.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
 const Color kGreenMainTheme = Color(0xff37c976);
+
+//Text style constants
+
+//page heading Font-Size
+
+const TextStyle kWhiteHeadingSize = TextStyle(
+    fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold); //white
+const TextStyle kBlackHeadingSize =
+    TextStyle(fontSize: 25, color: Colors.black); //black
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:busmate/Constants/constants.dart';
 
 void main() {
   runApp(const HomePage());
@@ -11,34 +11,121 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.redAccent,
-        body: Column(
-          children: [
-            Expanded(
-                child: Container(
-              height: double.infinity,
-              width: double.infinity,
-              color: Colors.redAccent,
-              child: Column(
-                children: [
-                  Row(
-                    children: [],
-                  )
-                ],
-              ),
-            )),
-            Expanded(
-                child: Container(
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(35),
-                      topLeft: Radius.circular(35))),
-              height: double.infinity,
-              width: double.infinity,
-            ))
-          ],
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: kGreenMainTheme,
+          body: Column(
+            children: [
+              Expanded(
+                  child: Container(
+                padding: const EdgeInsets.all(24),
+                height: double.infinity,
+                width: double.infinity,
+                color: kGreenMainTheme,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(bottom: 9),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                                flex: 3,
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Text(
+                                      'Hi, User Name',
+                                      style: kWhiteHeadingSize,
+                                    ),
+                                    Text(
+                                      '6 Jun, 2023',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: Color(0xFFE4E4E4)),
+                                    )
+                                  ],
+                                )),
+                            Expanded(
+                              child: CircleAvatar(
+                                radius: 50,
+                                backgroundColor: kGreenMainTheme,
+                                backgroundImage: Image.network(
+                                        'https://images.pexels.com/photos/213780/pexels-photo-213780.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+                                    .image,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Expanded(
+                      child: Container(
+                        width: double.infinity,
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.location_on,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                              Text(
+                                'Kochi Today',
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        flex: 4,
+                        child: Container(
+                            child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('./assets/rainy-icon.png'),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'Rainy',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFFE4E4E4)),
+                            )
+                          ],
+                        )))
+                  ],
+                ),
+              )),
+              Expanded(
+                  child: Container(
+                padding: const EdgeInsets.all(24),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(35),
+                        topLeft: Radius.circular(35))),
+                height: double.infinity,
+                width: double.infinity,
+              ))
+            ],
+          ),
         ),
       ),
     );
