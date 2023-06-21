d9fa9e6d6037d3739dc6397620e29d1a058190a8
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-06-21 16:43:16 +0530

    fixded partial error navigation error still exisit

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -10,6 +10,8 @@ import '../controller/bottomNavBarController.dart';
 
 class AuthService {
   final BottomNavBarController _controller = Get.find();
+  final _auth = FirebaseAuth.instance;
+  final _GoogleSignIn = GoogleSignIn();
 
   HandleAuthState() {
     return StreamBuilder(
@@ -25,22 +27,18 @@ class AuthService {
   }
 
   Future<void> signInWithGoogle(BuildContext context) async {
-    final GoogleSignInAccount? googleUser =
-        await GoogleSignIn(scopes: <String>["email"]).signIn();
-
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser!.authentication;
-
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-
     try {
-      final UserCredential userCredential =
-          await FirebaseAuth.instance.signInWithCredential(credential);
-      // User signed in successfully, navigate to the homepage
-      Get.off(() => HomePage());
+      final GoogleSignInAccount? googleSignInAccount =
+          await _GoogleSignIn.signIn();
+      if (googleSignInAccount != null) {
+        final GoogleSignInAuthentication googleSignInAuthentication =
+            await googleSignInAccount.authentication;
+        final AuthCredential authCredential = GoogleAuthProvider.credential(
+            accessToken: googleSignInAuthentication.accessToken,
+            idToken: googleSignInAuthentication.idToken);
+        await _auth.signInWithCredential(authCredential);
+        Get.off(() => HomePage());
+      }
     } catch (e) {
       // Handle sign-in error
       print('Error signing in with Google: $e');
@@ -51,6 +49,7 @@ class AuthService {
   void signOut() async {
     try {
       await FirebaseAuth.instance.signOut();
+      await _GoogleSignIn.signOut();
       FirebaseAuth.instance.authStateChanges().listen((User? user) {
         if (user == null) {
           Get.offAll(() => Login());
