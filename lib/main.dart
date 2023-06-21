b9483adad2070139f1cead5574f3de6b354e8b69
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-06-21 16:06:25 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
+import 'package:busmate/Constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:busmate/routes/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:busmate/services/auth_service.dart';
 import 'package:busmate/controller/bottomNavBarController.dart';
+import 'package:easy_splash_screen/easy_splash_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(BusMate());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: EasySplashScreen(
+        logo: Image.asset(
+          './assets/BM-logo.png',
+          scale: 0.7,
+        ),
+        logoWidth: 200,
+        backgroundColor: kGreenMainTheme,
+        durationInSeconds: 1,
+        navigator: BusMate(),
+      ),
+    ),
+  );
 }
 
 class BusMate extends StatelessWidget {
